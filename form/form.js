8aab8078c99becd7df7ba868369466021686342c
Alex Bogdanovsky <alex.bogdanovsky@gmail.com>
2017-03-28 17:16:53 +0300

    form

---
+++ form/form.js
@@ -0,0 +1,13 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
+var app = express
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('/from', upload.array(), function (req, res, next) {
+    console.log(req.body);
+    res.json(req.body);
+});
